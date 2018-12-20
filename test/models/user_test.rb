671ff2ab4e7fee4511aadcc75864eb03e90cd64f
tsubasa sahoda <tsubasa.070802@gmail.com>
2018-12-20 11:49:20 +0900

    fix test/users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+      @user = User.new(name: "Example User",email:"user@example.com",
+               password: "foobar", password_confirmation: "foobar")
   end
-
-  test "should be valid" do
-    assert @user.valid?
-  end
-end
-
-def Setup
-    @user = User.new(name: "Example User",email:"user@example.com",
-             password: "foobar", password_confirmation: "foobar")
-end
 
   test "should be valid" do
   assert @user.valid?
@@ -40,12 +32,14 @@ end
   end
 
   test "email validation should accept valid addresses" do
-   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                         first.last@foo.jp alice+bob@baz.cn]
-   valid_addresses.each do |valid_address|
-     @user.email = valid_address
-     assert @user.valid?, "#{valid_address.inspect} should be valid"
-   end
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
    test "email validation should reject invalid addresses" do
    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
@@ -78,4 +72,5 @@ end
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
  end
